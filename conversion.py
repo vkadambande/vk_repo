9b03257d6353ab29f1debf5c597a5ac066f0bd73
vkadambande <59906863+vkadambande@users.noreply.github.com>
2020-01-19 20:34:36 +0530

    Add files via upload
    conversion

---
+++ conversion.py
@@ -0,0 +1,16 @@
+i=10
+f=10.10
+c=10+10j
+b=True
+s='This is string'
+ns="1234567890"
+
+
+
+print("Conversion from float to  int - " , int(f))
+
+print("Conversion from complex to string - ",str(c))
+
+print('''Conversion from bool to int - ''',int(b))
+
+print("Conversion from number str to float -",float(ns))
